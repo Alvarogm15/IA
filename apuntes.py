c8e8c65b12f56f4bb4d6226f6c358205c4ba2da4
Alvarogm15 <alvaro.grimartinez@alum.uca.es>
2024-06-04 10:35:08 +0200

    Actualizar apuntes.py

---
+++ apuntes.py
@@ -0,0 +1,50 @@
+#Para importar en pyhton:
+from archivo import *
+
+#Para introducir un dato:
+jugador = int(input("Introduzca el dato"))
+
+#Condicionales if:
+if dato != 1:
+
+#Bucle for
+
+for in range(n):
+
+for jugada in jugadas:
+
+#Para definir diferentes posibilidades:
+
+match dato:
+	case 0:
+    	print("Empate")
+    case 1:
+    	print("Gana jugador 1")
+        
+#Definir una funcion:
+
+def Funcion():
+    
+#Clases:
+
+from dataclasses import dataclass
+import numpy as np
+
+class Nodo:
+    tablero: np.array
+    vacias: int
+    N: int
+
+    def __init__(self, tablero):
+        self.tablero = tablero
+        self.N = self.tablero.shape[0]
+        self.vacias = len(np.where(tablero == 0)[0])
+
+
+
+
+
+
+    
+
+        
